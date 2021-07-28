70632f0f156db40a3c578d090327f33a4ba9b34e
datph259@gmail.com <datph259@gmail.com>
2021-07-28 14:19:45 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LTWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult KhachHang()
+        {
+            return View();
+        }
+
+        public ActionResult SanPham()
+        {
+            return View();
+        }
+    }
+}
